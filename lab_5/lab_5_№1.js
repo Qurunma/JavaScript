81d103a27eaad777290b327dbbcf2e9877cc5de7
Qurunma <yaestgrut75@gmail.com>
2022-02-06 15:14:50 +0300

    lab_5

---
+++ lab_5/lab_5_№1.js
@@ -0,0 +1,25 @@
+let emailNotChecked = ["user1@mail.ru", "user2@gmail.com", "user3", "313213", "3213214@rambler.ru", "e131ea@yandex.com", "3213213", "user1"];
+let emailBlackList = ["user3", "313213", "3213213", "user1"];
+let emailWhiteList = [];
+let indexOfBlackList = [];
+
+emailWhiteList = filter(emailNotChecked, emailBlackList);
+
+function filter(emailNotChecked, emailBlackList) {
+    let index2 = 0;
+
+    for (let index in emailBlackList) {
+        indexOfBlackList.push(emailNotChecked.indexOf(emailBlackList[index]))
+    }
+    for (index = 0; index < emailNotChecked.length; index++) {
+        if (indexOfBlackList.includes(index)) {
+            continue;
+        }
+        else {
+            emailWhiteList[index2] = emailNotChecked[index];
+            index2++;
+        }
+    }
+    return emailWhiteList;
+}
+console.log(emailWhiteList)
