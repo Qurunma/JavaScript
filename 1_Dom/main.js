c4b282eb6d04940fb6860af03c7ae3b9b9f2afe9
Qurunma <yaestgrut75@gmail.com>
2022-02-21 21:01:00 +0300

    №1

--- 1_Dom/main.js
+++ 1_Dom/main.js
@@ -1,9 +1,28 @@
-let mass = [
-  document.createElement("option"),
-  document.createElement("option"),
-  document.createElement("option"),
-];
-for (let id in mass) {
-  mass[id].textContent = `Вариант ${parseInt(id) + 1}`;
-  vibor.append(mass[id]);
+let arrayOpt = [];
+for (let index = 0; index < 6; index++) {
+  arrayOpt[index] = {
+    value: `co${index + 1}`,
+    label: `Вариант выбора ${index + 1}`,
+  };
+}
+let selected = document.createElement("select");
+let selectReady = initialisatorOpt(arrayOpt);
+
+document.body.append(selectReady);
+
+function initialisatorOpt(arrayOpt) {
+  let optArr = [];
+  for (let index in arrayOpt) {
+    optArr[index] = document.createElement("option");
+  }
+  console.log(optArr);
+  for (let index in optArr) {
+    optArr[index].textContent = arrayOpt[index].label;
+    optArr[index].value = arrayOpt[index].value;
+  }
+  for (let index in optArr) {
+    selected.append(optArr[index]);
+  }
+  // seleeect.append
+  return selected;
 }
