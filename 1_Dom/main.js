09e4158f4b06c80a60b37a5528a355bb05eda165
Qurunma <81317971+Qurunma@users.noreply.github.com>
2022-02-21 20:15:03 +0300

    Delete 1_Dom directory

--- 1_Dom/main.js
+++
@@ -1,9 +0,0 @@
-let mass = [
-  document.createElement("option"),
-  document.createElement("option"),
-  document.createElement("option"),
-];
-for (let id in mass) {
-  mass[id].textContent = `Вариант ${parseInt(id) + 1}`;
-  vibor.append(mass[id]);
-}
