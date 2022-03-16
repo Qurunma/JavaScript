a672ac9921f324b9db155aaa2f8bf8e8459a3eeb
Qurunma <yaestgrut75@gmail.com>
2022-03-16 20:54:13 +0300

    Timer 2.0

--- 3_Dom/main.js
+++ 3_Dom/main.js
@@ -1,43 +1,56 @@
-const inputMinutes = document.querySelector(".inputMinutes");
-const inputSeconds = document.querySelector(".inputSeconds");
-const buttonSend = document.querySelector(".button");
-const buttonStop = document.querySelector(".buttonStop");
-let work = false;
+// const inputMinutes = document.querySelector(".inputMinutes");
+// const inputSeconds = document.querySelector(".inputSeconds");
+// const buttonSend = document.querySelector(".button");
+// const buttonStop = document.querySelector(".buttonStop");
+// let work = false;
 
-function timer() {
-  if (inputMinutes.value > 0) {
-    if (inputSeconds.value > 0) {
-      let seconds = inputSeconds.value - 1;
-      inputSeconds.value = seconds;
-    } else {
-      inputMinutes.value = inputMinutes.value - 1;
-      inputSeconds.value = 59;
-    }
-  } else if (inputSeconds.value >= 0 && inputMinutes.value == 0) {
-    let seconds = inputSeconds.value - 1;
-    inputSeconds.value = seconds;
-  } else {
-    clearInterval(intId);
-    work = false;
-  }
+// function timer() {
+//   if (inputMinutes.value > 0) {
+//     if (inputSeconds.value > 0) {
+//       let seconds = inputSeconds.value - 1;
+//       inputSeconds.value = seconds;
+//     } else {
+//       inputMinutes.value = inputMinutes.value - 1;
+//       inputSeconds.value = 59;
+//     }
+//   } else if (inputSeconds.value >= 0 && inputMinutes.value == 0) {
+//     let seconds = inputSeconds.value - 1;
+//     inputSeconds.value = seconds;
+//   } else {
+//     clearInterval(intId);
+//     work = false;
+//   }
+// }
+
+// buttonSend.addEventListener("click", () => {
+//   if (
+//     work !== true &&
+//     Number(inputMinutes.value) &&
+//     (Number(inputSeconds.value) ||
+//       inputSeconds.value == "" ||
+//       Number(inputSeconds.value) != Number)
+//   ) {
+//     work = true;
+//     intId = setInterval(timer, 1000);
+//   } else {
+//     alert("Incorrect input or program work now");
+//   }
+// });
+
+// buttonStop.onclick = () => {
+//   clearInterval(intId);
+//   work = false;
+// };
+
+const h2 = document.querySelector(".h");
+const inputer = document.querySelector(".inputer");
+let id;
+
+function addText() {
+  h2.textContent = inputer.value;
 }
 
-buttonSend.addEventListener("click", () => {
-  if (
-    work !== true &&
-    Number(inputMinutes.value) &&
-    (Number(inputSeconds.value) ||
-      inputSeconds.value == "" ||
-      Number(inputSeconds.value) != Number)
-  ) {
-    work = true;
-    intId = setInterval(timer, 1000);
-  } else {
-    alert("Incorrect input or program work now");
-  }
+inputer.addEventListener("input", () => {
+  clearTimeout(id);
+  id = setTimeout(addText, 300);
 });
-
-buttonStop.onclick = () => {
-  clearInterval(intId);
-  work = false;
-};
